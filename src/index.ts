dd71961b0cae7dfc3b8546466c419d4d30a4eb12
Alejandro Gallardo <agallardoc1@miumg.edu.gt>
2025-07-19 15:49:04 -0600

    modificando

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,25 @@ console.log(array);
 console.log(`El array tiene ${array.length} elementos.`);
 
 console.log(`El primer elemento del array es: ${array[0]}`);
+
+const objeto :{
+    nombre: string;
+    edad: number;
+} = {
+    nombre: 'Juan',
+    edad: 30
+};
+
+type Persona = typeof objeto;
+
+interface Animal {
+    nombre: string;
+    edad: number;
+}
+
+const animal: Animal = {
+    nombre: 'Perro',
+    edad: 5
+};
+
+console.log(`El animal se llama ${animal.nombre} y tiene ${animal.edad} años.`);
